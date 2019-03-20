14cb3273987616be241609d43897f8dc900143c2
Ross Fairbanks <ross@giantswarm.io>
2019-03-20 20:32:50 +0100

    Initial version with create cluster

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/giantswarm/microerror"
+	"sigs.k8s.io/kind/pkg/cluster"
+	"sigs.k8s.io/kind/pkg/cluster/config"
+	"sigs.k8s.io/kind/pkg/cluster/create"
+)
+
+func main() {
+	err := mainWithError()
+	if err != nil {
+		panic(fmt.Sprintf("%#v\n", err))
+	}
+}
+
+func mainWithError() (err error) {
+	ctx := cluster.NewContext("kind")
+	cfg := &config.Cluster{}
+
+	err = ctx.Create(cfg, create.Retain(true), create.WaitForReady(5*time.Minute))
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	return nil
+}
